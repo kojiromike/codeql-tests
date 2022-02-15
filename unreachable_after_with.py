cabc6d4cf4dff5670e559557d2281e71da8c1a37
Michael A. Smith <michael@smith-li.com>
2022-02-15 09:40:08 -0500

    Create unreachable_after_with.py

---
+++ unreachable_after_with.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+This file demonstrates that code inside a `with` block may not be executed, and flow can continue after the end of the block.
+"""
+
+from contextlib import suppress
+
+
+with suppress(ValueError):
+  raise ValueError("You knew this would happen")
+  
+print("but here we are")
